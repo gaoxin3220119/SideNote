209f866536181795dbe20c7413b20f3c915fe4b1
gaoxin3220119 <45986217+gaoxin3220119@users.noreply.github.com>
2023-10-09 18:36:58 +0800

    Delete shims.d.ts

--- shims.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.vue' {
-  import { ComponentOptions, DefineComponent } from 'vue';
-  // const componentOptions: ComponentOptions;
-  // export default componentOptions;
-  const defineComponent: DefineComponent;
-  export default defineComponent;
-}
